76a85135138768e401141c64541baa8fdd7bafaa
IngCampos <15151130@mail.ita.mx>
2019-04-12 08:51:53 -0500

    4 punto 10 cd, dvds, blu-ray, top9

--- application/views/home.php
+++ application/views/home.php
@@ -16,6 +16,27 @@
 	<p>Bienvenido a la pagina de inicio de <strong>CD Shop</strong>, disfruten su estancia.</p>
 	
 	<div id="widgets">
+	<h3>Top 9 de CDs mas vendidos</h3>
+		<?php foreach($aleatorios as $a): ?>
+			<div class="widget">
+				<h3><?php echo $a->nombre; ?></h3>
+				<a href="<?php echo base_url(); ?>principal/detalle_producto/<?php echo $a->id; ?>" 
+					title="Ver en catalogo">
+					<img src="<?php echo base_url(); ?>images/<?php echo $a->imagen; ?>" width="150" height="150">
+				</a>
+			</div>
+		<?php endforeach; ?>
+		<h3>Top 9 de DVDs mas vendidos</h3>
+		<?php foreach($aleatorios as $a): ?>
+			<div class="widget">
+				<h3><?php echo $a->nombre; ?></h3>
+				<a href="<?php echo base_url(); ?>principal/detalle_producto/<?php echo $a->id; ?>" 
+					title="Ver en catalogo">
+					<img src="<?php echo base_url(); ?>images/<?php echo $a->imagen; ?>" width="150" height="150">
+				</a>
+			</div>
+		<?php endforeach; ?>
+		<h3>Top 9 de BLU-RAY mas vendidos</h3>
 		<?php foreach($aleatorios as $a): ?>
 			<div class="widget">
 				<h3><?php echo $a->nombre; ?></h3>
