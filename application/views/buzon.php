0e5b619a025f59a1e959423fc06ed647bb78779b
IngCampos <15151130@mail.ita.mx>
2019-05-24 09:40:44 -0500

    Mejoria buzon
    Diseño como el panel de las estadisticas.
    Posibilidad de eilimar un registro utilizando metodo get.

--- application/views/buzon.php
+++ application/views/buzon.php
@@ -6,6 +6,11 @@ if($usuario->usuario!="admin1"){//validacion de que sea el usuario admin1
     }
 ?>
 <?php 
+if(isset($_GET["id_delete"])){// en caso de que se elimine una registro, aqui se recibo por get
+	$conexion2 = new PDO('mysql:host=localhost; dbname=cdshopco_ldstore2;','root','');
+	$statement2 = $conexion2->prepare("DELETE FROM quejas_sugerencias where id='".$_GET["id_delete"]."'");
+	$statement2->execute();	
+}
 $conexion2 = new PDO('mysql:host=localhost; dbname=cdshopco_ldstore2;','root','');
 $statement2 = $conexion2->prepare("SELECT * FROM quejas_sugerencias");
 $statement2->execute();
@@ -20,12 +25,17 @@ if (isset($_POST['eliminar'])){
 <center><h1>Buzon de quejas y sugerencias</h1></center>
 <div class="row jumbotron">
 <?php foreach($quejasSugerencias as $qs): ?>
-<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-<h5> Id_Queja 
-<?php " ". $qs['id'].": "; if($qs['usuario']!=""){
-    echo $qs['usuario']." "; echo $qs['apellidos']."("; echo $qs['correo'].")";}?>
-	</h5>
-<?php echo $qs['titulo'].": "; echo $qs['descripcion']; ?>
-</div>
+<div class="card border-primary col-sm-12 col-md-6 col-lg-4 col-xl-3 card">
+		<div class="card-header"><center><?php echo $qs['usuario']." "; echo $qs['apellidos']."("; echo $qs['correo'].")";?></center></div>
+        <div class="card-body text-primary">
+            <h5 class="card-title"><?php echo $qs['titulo'].": ";?></h5>
+            <p class="card-text">
+			<?php echo $qs['descripcion']; ?>
+            </p>
+			<hr>
+			<center><a class="btn btn-danger" href="<?php echo base_url()."principal/buzon?id_delete=".$qs['id']; ?>">Eliminar</a></center>
+        </div>
+    </div>
 <?php endforeach; ?><br>
+</div>
 </div>
