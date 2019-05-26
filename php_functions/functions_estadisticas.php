1c42de3c91e945688d5b7ef87f50636c8d45fafc
IngCampos <15151130@mail.ita.mx>
2019-05-26 12:36:12 -0500

    Actualizacion estadisticas 2,3,4,10 y 11
    Implementado correctamente la capa de seguridad, en estas graficas se extrae la informacion de la base de datos original(la que le da funcionamiento al sistema).
    La tabla datagen de ldstore3 queda invalida para las estadisticas.

--- php_functions/functions_estadisticas.php
+++ php_functions/functions_estadisticas.php
@@ -18,9 +18,10 @@ $estadistica16valor = "";
 $estadistica17valor = "";
 $estadistica18valor = "";
 // Conexion a la base de datos para obtener la informacion para las graficas
-$conexion = new PDO('mysql:host=localhost; dbname=cdshopco_ldstore3;','root','');
+$conexion2 = new PDO('mysql:host=localhost; dbname=cdshopco_ldstore2;','root','');
+$conexion3 = new PDO('mysql:host=localhost; dbname=cdshopco_ldstore3;','root','');
 //----
-$statement2 = $conexion->prepare("SELECT MOB_DESK, COUNT(MOB_DESK) AS REPETICIONES FROM datagen AS a WHERE DATE BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY MOB_DESK");
+$statement2 = $conexion2->prepare("SELECT Tipo, COUNT(Tipo) AS REPETICIONES FROM dispositivos AS a WHERE Ultimo_Acceso BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY Tipo");
 //Consulta SQL, generando el numero de repeticiones, y  validando que las fechas sean entre las que se pusieron en el form
 $statement2->execute();
 $estadistica2 = $statement2->fetchAll();
@@ -30,11 +31,11 @@ $total= $total+$valor["REPETICIONES"];//obtener el valor total para determinar e
 }
 foreach($estadistica2 as $valor){
   $valor["PORCENTAJE"] =  (round($valor["REPETICIONES"]/($total), 2)*100);//asignacion de porcentaje
-  $estadistica2valor.= "{label: '".$valor["MOB_DESK"]."(".$valor["REPETICIONES"].")', value: ".$valor["PORCENTAJE"]."},";//valores que iran en el arreglo para la grafica
+  $estadistica2valor.= "{label: '".$valor["Tipo"]."(".$valor["REPETICIONES"].")', value: ".$valor["PORCENTAJE"]."},";//valores que iran en el arreglo para la grafica
 }
 $estadistica2valor = substr ($estadistica2valor, 0, -1);//se elimina el ultimo caracter que es una , que marcaria error
 //----
-$statement3 = $conexion->prepare("SELECT estad_paginas.NOMBRE_PAGINA, COUNT(estad_paginas.ID_PAGINA) AS REPETICIONES FROM estadisticas_paginas INNER JOIN estad_paginas ON estadisticas_paginas.ID_PAGINA=estad_paginas.ID_PAGINA WHERE FECHA BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY estad_paginas.ID_PAGINA");
+$statement3 = $conexion3->prepare("SELECT estad_paginas.NOMBRE_PAGINA, COUNT(estad_paginas.ID_PAGINA) AS REPETICIONES FROM estadisticas_paginas INNER JOIN estad_paginas ON estadisticas_paginas.ID_PAGINA=estad_paginas.ID_PAGINA WHERE FECHA BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY estad_paginas.ID_PAGINA");
 $statement3->execute();
 $estadistica3 = $statement3->fetchAll();
 $total=0;
@@ -47,7 +48,7 @@ foreach($estadistica3 as $valor){
 }
 $estadistica3valor = substr ($estadistica3valor, 0, -1);
 //----
-$statement4 = $conexion->prepare("SELECT DATE, COUNT(DATE) AS REPETICIONES FROM datagen AS a WHERE DATE BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY DATE");
+$statement4 = $conexion2->prepare("SELECT Ultimo_Acceso, COUNT(Ultimo_Acceso) AS REPETICIONES FROM dispositivos AS a WHERE Ultimo_Acceso BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY Ultimo_Acceso");
 $statement4->execute();
 $estadistica4 = $statement4->fetchAll();
 $total=0;
@@ -56,11 +57,11 @@ $total= $total+$valor["REPETICIONES"];
 }
 foreach($estadistica4 as $valor){
   $valor["PORCENTAJE"] =  (round($valor["REPETICIONES"]/($total), 2)*100);
-  $estadistica4valor.= "{x: '".$valor["DATE"]."', y: ".$valor["REPETICIONES"].",z:".$valor["PORCENTAJE"]."},";
+  $estadistica4valor.= "{x: '".$valor["Ultimo_Acceso"]."', y: ".$valor["REPETICIONES"].",z:".$valor["PORCENTAJE"]."},";
 }
 $estadistica4valor = substr ($estadistica4valor, 0, -1);
 //----
-$statement10 = $conexion->prepare("SELECT OS, COUNT(OS) AS REPETICIONES FROM datagen AS a WHERE DATE BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY OS");
+$statement10 = $conexion2->prepare("SELECT SO, COUNT(SO) AS REPETICIONES FROM dispositivos AS a WHERE Ultimo_Acceso BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY SO");
 $statement10->execute();
 $estadistica10 = $statement10->fetchAll();
 $total=0;
@@ -69,11 +70,11 @@ $total= $total+$valor["REPETICIONES"];
 }
 foreach($estadistica10 as $valor){
   $valor["PORCENTAJE"] =  (round($valor["REPETICIONES"]/($total), 2)*100);
-  $estadistica10valor.= "{label: '".$valor["OS"]."(".$valor["REPETICIONES"].")', value: ".$valor["PORCENTAJE"]."},";
+  $estadistica10valor.= "{label: '".$valor["SO"]."(".$valor["REPETICIONES"].")', value: ".$valor["PORCENTAJE"]."},";
 }
 $estadistica10valor = substr ($estadistica10valor, 0, -1);
 //----
-$statement11 = $conexion->prepare("SELECT BROWSER, COUNT(BROWSER) AS REPETICIONES FROM datagen AS a WHERE DATE BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY BROWSER");
+$statement11 = $conexion2->prepare("SELECT Navegador, COUNT(Navegador) AS REPETICIONES FROM dispositivos AS a WHERE Ultimo_Acceso BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY Navegador");
 $statement11->execute();
 $estadistica11 = $statement11->fetchAll();
 $total=0;
@@ -82,11 +83,11 @@ $total= $total+$valor["REPETICIONES"];
 }
 foreach($estadistica11 as $valor){
   $valor["PORCENTAJE"] =  (round($valor["REPETICIONES"]/($total), 2)*100);
-  $estadistica11valor.= "{label: '".$valor["BROWSER"]."(".$valor["REPETICIONES"].")', value: ".$valor["PORCENTAJE"]."},";
+  $estadistica11valor.= "{label: '".$valor["Navegador"]."(".$valor["REPETICIONES"].")', value: ".$valor["PORCENTAJE"]."},";
 }
 $estadistica11valor = substr ($estadistica11valor, 0, -1);
 //----
-$statement18 = $conexion->prepare("SELECT TERMINO, COUNT(TERMINO) AS REPETICIONES FROM busquedas AS a WHERE FECHA BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY TERMINO");
+$statement18 = $conexion3->prepare("SELECT TERMINO, COUNT(TERMINO) AS REPETICIONES FROM busquedas AS a WHERE FECHA BETWEEN '$inicio[0] $inicio[1]:00' AND '$final[0] $final[1]:00' GROUP BY TERMINO");
 $statement18->execute();
 $estadistica18 = $statement18->fetchAll();
 $total=0;
